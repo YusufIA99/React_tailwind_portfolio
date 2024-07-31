1990687fd3a4859c5e310acd9fb4150d0e094971
YusufIA99 <yullemanden@gmail.com>
2024-07-31 23:15:31 +0200

    navigationbar css

--- src/pages/navigation.jsx
+++ src/pages/navigation.jsx
@@ -1,13 +1,28 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 function Navigation() {
   return (
-    <nav className="bg-black text-white p-4 flex justify-center items-center">
+    <nav className="bg-black text-gray-400 p-4 flex justify-center items-center">
       <div className="bg-gray-800 rounded-full px-1 py-2">
-        <Link to="/" className="hover:text-gray-400 px-3">Home</Link>
-        <Link to="/projects" className="hover:text-gray-400 px-3">Projects</Link>
-        <Link to="/contact" className="hover:text-gray-400 px-3">Contact</Link>
+        <NavLink 
+          to="/" 
+          className={({ isActive }) => isActive ? "text-white px-3" : "hover:text-gray-400 px-3"}
+        >
+          Home
+        </NavLink>
+        <NavLink 
+          to="/projects" 
+          className={({ isActive }) => isActive ? "text-white px-3" : "hover:text-gray-400 px-3"}
+        >
+          Projects
+        </NavLink>
+        <NavLink 
+          to="/contact" 
+          className={({ isActive }) => isActive ? "text-white px-3" : "hover:text-gray-400 px-3"}
+        >
+          Contact
+        </NavLink>
       </div>
     </nav>
   );

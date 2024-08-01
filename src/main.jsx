5fcdd23ea610e98e78230f8a0058447f5c6a33b0
YusufIA99 <yullemanden@gmail.com>
2024-08-01 23:03:25 +0200

    github pages

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/React_tailwind_portfolio">
       <Navigation />
       <Routes>
         <Route path="/" element={<Home />} />

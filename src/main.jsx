3e2b75feed594e713e6cb5918f8b8b35cc192d9c
YusufIA99 <yullemanden@gmail.com>
2024-12-11 13:24:08 +0100

    changed router

--- src/main.jsx
+++ src/main.jsx
@@ -6,13 +6,13 @@ import Navigation from './pages/navigation.jsx';
 import Home from './pages/home.jsx';
 import Projects from './pages/projects.jsx';
 import Contact from './pages/contact.jsx';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from './pages/themecontext.jsx';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider>
-      <BrowserRouter basename="/">
+      <HashRouter>
         <Navigation />
         <Routes>
           <Route path="/" element={<Home />} />
@@ -20,7 +20,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
           <Route path="/app" element={<App />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </ThemeProvider>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
